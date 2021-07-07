11cadf7a2d68e9929808a0d411324f2a3f01b631
Uladzimir Soltan <soltanvladimir@yahoo.com>
2021-07-07 10:16:40 +0200

    Add Hoc and Fix Homework 4

--- RainbowFrame/Components/RainbowFrame.jsx
+++ RainbowFrame/Components/RainbowFrame.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './RainbowFrame.css';
 
-export default props => {
+export const RainbowFrame = props => {
     return (
         <div className="RainbowFrame">
             {props.colors.reduce((prev, color) => <div style={{borderColor: color}}>{prev}</div>, props.children)}
--- RainbowFrameHOC/App.js
+++ RainbowFrameHOC/App.js
@@ -2,13 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {RainbowFrame} from './components/RainbowFrame.jsx';
 
-let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
-
 ReactDOM.render(
-  <RainbowFrame colors={colors}>
+  <RainbowFrame>
       Hello!
   </RainbowFrame>, document.getElementById('container') 
 );
